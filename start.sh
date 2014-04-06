67156f8dfe1a4cbef2a75977468050a833acc8ca
李 俊良 <junguoliang@163.com>
2014-04-06 12:29:13 +0800

    数据表结构建立

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-PORT=87 nohup forever app.js 2>&1 | /usr/local/sbin/cronolog "./log/log_s000_%Y%m%d.log" 2>&1 &
+PORT=3000 nohup forever app.js 2>&1 | /usr/local/sbin/cronolog "./log/log_s000_%Y%m%d.log" 2>&1 &
